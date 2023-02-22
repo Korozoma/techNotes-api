3d92125eadd025e7a352fc86408be6e80ac6e258
Renie Ver Basilio <104410029+Korozoma@users.noreply.github.com>
2023-02-23 02:07:48 +0800

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://technotes-6ox1.onrender.com'
-    'https://technotes-api-fax3.onrender.com/'
+    'https://technotes-api-fax3.onrender.com'
 ]
 
 module.exports = allowedOrigins
